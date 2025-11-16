b44bd9f96d72d46338bfa506d52a4d00bcdf013f
Mukhammedali <241918@astanait.edu.kz>
2025-11-16 21:46:50 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,53 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        //Scanner для получения ввода пользователя
-        Scanner scanner = new Scanner(System.in);
-        //Переменные классов
-        Store store = new Store();
-        Cart cart = new Cart();
+    public static void main(String[] args) {
 
-        while(true) {
-            System.out.println("\nWelcome to E-Commerce Store!");
-            System.out.println("Choose option\n 1. Manage product (Admin)\n2. Buy product (User)\n3. Exit");
-            int choice = scanner.nextInt();
+        Product asus=ProductFactory.create("laptop");
+        Product apple=ProductFactory.create("phone");
+
+        System.out.println("        Standard Products");
+        asus.show();  apple.show();
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("\n    Custom Laptop");
+
+        try {
+            double cRam=0;
+            double cSsd=0;
+            String cColor="";
+
+            System.out.print("Enter RAM: ");
+            cRam=scanner.nextDouble();
+            if (cRam<=0){
+                throw new Exception("RAM must be greater than 0");
+            }
+
+            System.out.print("Enter SSD: ");
+            cSsd=scanner.nextDouble();
+if(cSsd<=0){
+    throw new Exception("SSD must be greater than 0");
+}
+
             scanner.nextLine();
 
-            switch(choice) {
-                case 1:
-                    System.out.println("Welcome to Admin menu!\n" +
-                            "Choose option:\n" +
-                            "1. Add product\n" +
-                            "2. Change product price\n" +
-                            "3. Change product stock\n" +
-                            "4. List products\n" +
-                            "5. Exit\n");
-                    int option = scanner.nextInt();
-                    scanner.nextLine();
-                    switch(option) {
-                        case 1:
-                            //Добавляем товар через ProductBuilder
-                            System.out.println("Enter product name:");
-                            String name = scanner.nextLine();
-                            System.out.println("Enter price:");
-                            double price = scanner.nextDouble();
-                            System.out.println("Enter stock:");
-                            int stock = scanner.nextInt();
-                            scanner.nextLine();
+            System.out.print("Enter color: ");
+            cColor=scanner.nextLine();
+            Laptop customLaptop=(Laptop) new Laptop.LaptopBuilder()
+                    .setRam(cRam)
+                    .setssd(cSsd)
+                    .setcolor(cColor)
+                    .build();
 
-                            Product product = new ProductBuilder().setName(name).setPrice(price).setStock(stock).build();
+            System.out.println("\n ");
+            customLaptop.show();
 
-                            store.addProduct(p);
-                    }
-
-
-                case 2:
-
-                case 3:
-                    System.out.println("Exiting...");
-                    return;
-                default: System.out.println("Invalid option!");
-            }
         }
+        catch (Exception e){
+            System.out.println("\n error input ");  }
+        finally {
+            scanner.close();
+           }
     }
 }
