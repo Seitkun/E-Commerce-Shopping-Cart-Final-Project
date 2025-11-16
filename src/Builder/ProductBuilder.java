2b3eb15dd7c7fe5462963a95ada3bab1fb0ae3a0
Mukhammedali <241918@astanait.edu.kz>
2025-11-16 23:49:10 +0500

    Update ProductBuilder.java

--- src/Builder/ProductBuilder.java
+++ src/Builder/ProductBuilder.java
@@ -9,22 +9,23 @@ public class ProductBuilder {
     private String type;
 
     public ProductBuilder setName(String name) {
-        this.name = name;
+        this.name=name;
         return this;
     }
     public ProductBuilder setPrice(double price) {
-        this.price = price;
+        this.price=price;
         return this;
     }
     public ProductBuilder setStock(int stock) {
-        this.stock = stock;
+        this.stock=stock;
         return this;
     }
     public ProductBuilder setType(String type) {
-        this.type = type;
+        this.type=type;
         return this;
     }
     public Product build() {
         return new Product(name, price, stock, type);
     }
 }
+
