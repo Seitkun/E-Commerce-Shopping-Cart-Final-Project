916e7881cb59887f9356298415342de1e62f838b
Mukhammedali <241918@astanait.edu.kz>
2025-11-16 23:46:30 +0500

    Update Store.java

--- src/Classes/Store.java
+++ src/Classes/Store.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Store extends StoreSubject {
-    private final List<Product> products = new ArrayList<>();
+    private final List<Product> products=new ArrayList<>();
 
     public void addProduct(Product product) {
         products.add(product);
@@ -14,23 +14,24 @@ public class Store extends StoreSubject {
     }
 
     public void changePrice(int id, double newPrice) {
-        Product product = products.get(id);
+        Product product=products.get(id);
         product.setPrice(newPrice);
         notifyObservers(product);
     }
 
     public void changeStock(int id, int newStock) {
-        Product product = products.get(id);
+        Product product=products.get(id);
         product.setStock(newStock);
         notifyObservers(product);
     }
 
     public void list(){
-        for (int i = 0; i < products.size(); i++) {
+        for (int i=0; i<products.size(); i++) {
             System.out.println((i + 1) + ") " + products.get(i));
-        }
+         }
     }
     public Product get(int id) {
         return products.get(id);
-    }
+    
+      }
 }
