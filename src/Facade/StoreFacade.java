3c3b1256249aee624a93695d4b6037c037277485
Mukhammedali <241918@astanait.edu.kz>
2025-11-16 23:37:09 +0500

    Update StoreFacade.java

--- src/Facade/StoreFacade.java
+++ src/Facade/StoreFacade.java
@@ -3,6 +3,7 @@ package Facade;
 import Classes.*;
 import Builder.*;
 import Strategy.*;
+
 import Prototype.*;
 
 public class StoreFacade {
@@ -10,8 +11,8 @@ public class StoreFacade {
     private final Cart cart;
 
     public StoreFacade(Store store, Cart cart) {
-        this.store = store;
-        this.cart = cart;
+        this.store=store;
+        this.cart=cart;
     }
 
     public void adminAddProduct(String name, double price, int stock) {
@@ -31,8 +32,13 @@ public class StoreFacade {
                 store.get(id).getName(),
                 store.get(id).getPrice(),
                 store.get(id).getStock(),
+            
                 store.get(id).getType()
         );
         store.addProduct(original.clone());
-    }
+   
+ }
+
 }
+
+
